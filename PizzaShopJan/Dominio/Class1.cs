00b8e6083412d8e0a3fa51525e9a6400e78fd59f
Antonio Javier Pérez Rodríguez <ajpr1993@hotmail.com>
2018-04-11 23:51:14 +0200

    Añadido intento de Dominio

---
+++ PizzaShopJan/Dominio/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dominio
+{
+    public class Pizza
+    {
+        public List<string> ingredientes = new List<string>() { };
+        public List<string> comments = new List<string>() { };
+        public string Name { get; set; }
+    }
+
+    public interface IRepository
+    {
+        void Add(Pizza pizza);
+        void Update(Pizza pizza);
+    }
+}
